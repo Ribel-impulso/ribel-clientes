256e8453c28108c1ab03186083c55d90dc99f0e8
Maribel <maribelefontana@gmail.com>
2026-05-30 23:30:38 -0300

    fix supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
-impoimport { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseKey)rt { createClient } from '@supabase/supabase-js'
+export const supabase = createClient(supabaseUrl, supabaseKey)
